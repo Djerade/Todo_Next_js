1b0188ff60185468204c2a9c910b6f524fc407c6
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2023-10-25 08:14:04 +0200

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = 5000
+
+app.get('/',(req,res) =>{
+    res.send("salut")
+})
+
+app.listen(PORT, () => {
+    console.log('serveur function');
+})
+
