aee7ec11d1b39370db500a9c13131bc30d3b1593
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2023-11-21 12:22:30 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const { graphqlHTTP } = require('express-graphql');
 const express = require('express');
 
+require('dotenv').Config()
+
 const { db } = require('./Config/db');
 
 //Port
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 //initalisation du serveur
 const app = express();
@@ -13,7 +15,7 @@ const app = express();
 app.get('/',(req,res) =>{
     res.send("hello word")
 })
-//Teste de rout
+//Teste de rout                                     
 app.listen(PORT, () => {
     console.log('serveur running');
 })
