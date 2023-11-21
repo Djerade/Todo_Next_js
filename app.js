44213eb9ff60b190d95f2aa87808436c2c90e74f
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2023-11-21 21:39:06 +0100

    update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.listen(PORT, () => {
 app.use('/graphql', graphqlHTTP({
     schema,
     rootValue: resolver,
-    graphiql: process.env.NODE_ENV === 'devlopment'
+    graphiql: true
 }));
 
 
